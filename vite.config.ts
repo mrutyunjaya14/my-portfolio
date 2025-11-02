f25f3f27d682fbbcf7ac9d662656e1b0a88ceffd
mrutyunjaya14 <mrutyunjayabeura2004@gmail.com>
2025-11-02 18:44:07 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+   base: "/myportfolio/",
   server: {
     host: "::",
     port: 8080,
